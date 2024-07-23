7bb1316831b27553e8fe1c35d43d64d0eac1f0d8
ericqn <ericqn1234@gmail.com>
2024-07-23 15:36:23 -0700

    included slurm file for more gpu time

---
+++ slurm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -J Tabular        # Job name
+#SBATCH -o /path/tabular.%j.log   # define stdout filename; %j expands to jobid; to redirect stderr elsewhere, duplicate this line with -e instead
+#SBATCH --mail-user=YOUREMAIL
+#SBATCH --mail-type=FAIL,TIME_LIMIT # get notified via email on job failure or time limit reached
+#SBATCH --partition=gpuA100x4         # specify queue, if this doesn’t submit try gpu-shared
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=8
+#SBATCH --account=bbjr-delta-gpu
+#SBATCH --mem=40G
+#SBATCH --tasks=1              # Number of nodes, not cores (16 cores/node)
+#SBATCH --nodes=1             # Total number of MPI tasks (if omitted, n=N); this is 1 for us
+#SBATCH --time=24:00:00       # set maximum run time in H:m:S
+#SBATCH --no-requeue     # don’t automatically requeue job if node fails, usually errors need to be inspected and debugged
+# Load Anaconda module
+module --ignore_cache load “anaconda3”
+# Initialize conda
+eval “$(conda shell.bash hook)”
+# Activate the desired conda environment
+conda activate flk_6
+# Ensure the conda environment is activated properly
+echo “Activated conda environment: $(conda info --envs | grep ‘*’ | awk ‘{print $1}’)”
+# Run the training script
+python path/train_cifar10.py --net simple_model_9 --n_epochs 200 --n_layers 6 --n_heads 8 --bs 64 --dimhead 64
