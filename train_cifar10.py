ae223e693473e4d7dbc67e4dd4fc00189164f3e4
ericqn <ericqn1234@gmail.com>
2024-08-29 17:32:53 -0700

    transformer bug fixes 2

--- train_cifar10.py
+++ train_cifar10.py
@@ -198,14 +198,14 @@ class Dataloader:
 # previous dataloader
 transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
-    transforms.Resize(args.size),
+    transforms.Resize(size),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 transform_test = transforms.Compose([
-    transforms.Resize(args.size),
+    transforms.Resize(size),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
