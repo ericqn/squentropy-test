93f014a7b7d11e478d744d639340e2395205fea2
ericqn <ericqn1234@gmail.com>
2024-09-03 18:11:01 -0700

    small bug fix dataloader

--- train_cifar10.py
+++ train_cifar10.py
@@ -122,7 +122,7 @@ class Dataloader:
                 transforms.ToTensor(),
                 transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
             ])
-        if (dataset_arg == "svhn"):
+        elif (dataset_arg == "svhn"):
             transform_train = transforms.Compose([
                 transforms.RandomCrop(32, padding=4),
                 transforms.RandomHorizontalFlip(),
